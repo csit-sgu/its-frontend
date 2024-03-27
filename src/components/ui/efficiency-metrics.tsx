8b2400ab5a108cbf42d91ceeedd3beeabc162b8a
Artem Kuydakov <t3m8ch@yandex.ru>
2024-03-27 23:11:38 +0400

    feat: Create fish for metrics components

---
+++ src/components/ui/efficiency-metrics.tsx
@@ -0,0 +1,7 @@
+export function EfficiencyMetrics({ relative, absolute }: { relative: number; absolute: number }) {
+  return (
+    <div>
+      efficiency metrics: {relative} {absolute}
+    </div>
+  );
+}
---
+++ src/components/ui/good-bad-metrics.tsx
@@ -0,0 +1,7 @@
+export function GoodBadMetrics({ good, bad }: { good: number; bad: number }) {
+  return (
+    <div>
+      Good bad metrics: {good} {bad}
+    </div>
+  );
+}
