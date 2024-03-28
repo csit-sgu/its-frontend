f6d521b1e8e876a012ecd19a78a7e36332b6abf3
Artem Kuydakov <t3m8ch@yandex.ru>
2024-03-28 13:50:48 +0400

    feat: Implement change region button

--- src/app/regions/[regionId]/page.tsx
+++ src/app/regions/[regionId]/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { TaskItem } from '@/components/entities/task/task-item';
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { EfficiencyMetrics } from '@/components/ui/efficiency-metrics';
 import { GoodBadMetrics } from '@/components/ui/good-bad-metrics';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { TaskType } from '@/domain/types';
 import dayjs from 'dayjs';
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
 function processTaskTypesParam(param: string | null): TaskType[] {
@@ -28,9 +29,9 @@ export default function RegionProfile({ params }: { params: { regionId: string }
     <div className="container">
       <div className="flex pt-5 pb-5 justify-center">
         <h2 className="font-bold text-4xl">Саратовская область</h2>
-        <Button className="ml-2" variant="outline">
+        <Link href="/regions" className={buttonVariants({ variant: 'outline' }) + ' ml-2'}>
           Сменить регион
-        </Button>
+        </Link>
       </div>
       <div className="flex">
         <div className="w-[70%] pr-4">
